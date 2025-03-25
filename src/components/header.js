effb63a7dbe11931c20dfea26cb5b6262ed1ba01
Arseniy <burkoff.ars@yandex.ru>
2025-03-25 22:37:11 +0300

    about

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import loading from '../icons/52462ec6ba8380b43ebf17cb77c2880d.png';
 import avatar from '../img/Аватарка по умолчанию.jpg';
 import axios from "axios";
+import logo from '../icons/a21cf0e37634bf7f262554f86388e0d5.png';
+
 
 class Header extends React.Component {
     token = null;
@@ -50,7 +52,7 @@ class Header extends React.Component {
         return (
             <header>
                 <img
-                    src='https://s3-alpha-sig.figma.com/img/01e7/240a/a21cf0e37634bf7f262554f86388e0d5?Expires=1741564800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=UU0T2W2dDC16euVgXlidD8YUt7ReQGanRXgnaZstVlf7OjqqJRjQ8Bb3XSMowPlczIo99RGeXbtezL~~JWc8Q4g~Y0QQwjO8Mm-EK5UjOtCnLC6KqKf5Rolzj3T0-cwmGU~cx~I~JtgEvimyafjkCPFnejiJ2lSLu~iE4P20dmsnCeCUkCY8EiGy~WtffHIWDWZ27asVJExXC3vEHitCI7x3uZRhky-Meh9C~2FFAZyQn0W7jnUZ39tTlqUnw0-dpdSXcF9d5Hn4zXo0gEs-mB4mUL5DCVGGALuNOHIYKtehq1bI4rXi1X9gmZUVe7yNsYgDlIFcrgIMLr6OQHkjHA__'
+                    src= {logo}
                     className='logo'
                 />
                 <span className="info">
